193a284d4f05cef719458e606079abe57d2f5fd9
rolf-cains <rolf.cains@gmail.com>
2026-02-18 12:44:39 +0000

    fix database storage

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -2,7 +2,7 @@ import { Card } from '@/app/ui/dashboard/cards';
 import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import { lusitana } from '@/app/ui/fonts';
-import {  fetchCardData } from '@/app/lib/data';
+import {  fetchCardData, fetchLatestInvoices, fetchRevenue  } from '@/app/lib/data';
 import { Suspense } from 'react';
 import { RevenueChartSkeleton } from '@/app/ui/skeletons';
 import { LatestInvoicesSkeleton } from '@/app/ui/skeletons';
@@ -13,8 +13,8 @@ import { LatestInvoicesSkeleton } from '@/app/ui/skeletons';
 // }
 
 export default async function Page() {
-    //  const revenue = await fetchRevenue();
-    //  const latestInvoices = await fetchLatestInvoices();
+     const revenue = await fetchRevenue();
+     const latestInvoices = await fetchLatestInvoices();
      const {totalPaidInvoices, totalPendingInvoices, numberOfInvoices, numberOfCustomers} = await fetchCardData();
   return (
     <main>
