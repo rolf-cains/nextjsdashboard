a4d0bf2d1844074500f6298ded822c1117258d22
rolf-cains <rolf.cains@gmail.com>
2026-02-25 11:58:08 +0000

    updating to remove try/catch statement for error in ch12

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -29,19 +29,19 @@ export async function createInvoice(formData: FormData) {
   // Test it out:
   // console.log(customerId, amount, status);
 
-  try {
+  // try {
     await sql`
     INSERT INTO invoices (customer_id, amount, status, date)
     VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
   `;
-  } catch (error) {
-    // We'll also log the error to the console for now
-    console.error(error);
-    return {
-      message: 'Database Error: Failed to Create Invoice.',
+  // } catch (error) {
+  //   // We'll also log the error to the console for now
+  //   console.error(error);
+  //   return {
+  //     message: 'Database Error: Failed to Create Invoice.',
 
-    };
-  }
+  //   };
+  // }
     revalidatePath('/dashboard/invoices');
     redirect('/dashboard/invoices');
   
@@ -58,17 +58,17 @@ export async function updateInvoice(id: string, formData: FormData) {
  
   const amountInCents = amount * 100;
  
-  try {
+  // try {
   await sql`
     UPDATE invoices
     SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
     WHERE id = ${id}
   `;
-  } catch (error){
-        // We'll also log the error to the console for now
-    console.error(error);
-    return { message: 'Database Error: Failed to Update Invoice.' };
-  }
+  // } catch (error){
+  //       // We'll also log the error to the console for now
+  //   console.error(error);
+  //   return { message: 'Database Error: Failed to Update Invoice.' };
+  // }
   revalidatePath('/dashboard/invoices');
   redirect('/dashboard/invoices');
 }
